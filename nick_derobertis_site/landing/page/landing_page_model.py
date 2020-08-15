488d1835ecc965c83dfbb3c5b4ba4e92b7b9ef71
Nick DeRobertis <whoopnip@gmail.com>
2020-08-15 19:47:58 -0400

    Layout basically working with new component template structure

--- nick_derobertis_site/landing/page/landing_page_model.py
+++ nick_derobertis_site/landing/page/landing_page_model.py
@@ -1,6 +1,7 @@
 from typing import List
 
 import param
+from panel import Row
 
 from nick_derobertis_site.common.model import ComponentModel
 from nick_derobertis_site.landing.components.card.card_component import CardComponent
@@ -9,7 +10,7 @@ from nick_derobertis_site.landing.components.card.card_model import CardModel
 
 class LandingPageModel(ComponentModel):
     card_models = param.List(class_=CardModel)
-    cards = param.List(class_=CardComponent)
+    cards = param.ClassSelector(class_=Row)
 
     def __init__(self, **params):
         super().__init__(**params)
@@ -17,4 +18,4 @@ class LandingPageModel(ComponentModel):
 
     @param.depends('card_models', watch=True)
     def _set_cards(self):
-        self.cards = [CardComponent(model=mod) for mod in self.card_models]
+        self.cards = Row(*[CardComponent(model=mod) for mod in self.card_models])
