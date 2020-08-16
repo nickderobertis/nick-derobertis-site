b0c84e7247bb1a4d7a5fa5f9d38c49beeb1d7ce3
Nick DeRobertis <whoopnip@gmail.com>
2020-08-16 15:47:35 -0400

    Get original bootstrap layout working again now that component framework is basically working

--- nick_derobertis_site/landing/page/landing_page_model.py
+++ nick_derobertis_site/landing/page/landing_page_model.py
@@ -1,7 +1,6 @@
 from typing import List
 
 import param
-from panel import Row
 
 from nick_derobertis_site.common.model import ComponentModel
 from nick_derobertis_site.landing.components.card.card_component import CardComponent
@@ -10,7 +9,7 @@ from nick_derobertis_site.landing.components.card.card_model import CardModel
 
 class LandingPageModel(ComponentModel):
     card_models = param.List(class_=CardModel)
-    cards = param.ClassSelector(class_=Row)
+    cards = param.List(class_=CardComponent)
 
     def __init__(self, **params):
         super().__init__(**params)
@@ -18,4 +17,4 @@ class LandingPageModel(ComponentModel):
 
     @param.depends('card_models', watch=True)
     def _set_cards(self):
-        self.cards = Row(*[CardComponent(model=mod) for mod in self.card_models])
+        self.cards = [CardComponent(model=mod) for mod in self.card_models]
