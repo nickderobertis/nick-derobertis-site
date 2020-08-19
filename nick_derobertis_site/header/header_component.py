fd725abcc9f622ec51c6b6fc3b8b7f6884c12ff1
Nick DeRobertis <whoopnip@gmail.com>
2020-08-19 06:37:50 -0400

    Use primary button in header instead of panel button

--- nick_derobertis_site/header/header_component.py
+++ nick_derobertis_site/header/header_component.py
@@ -7,18 +7,20 @@ from param.parameterized import Event
 
 from nick_derobertis_site.common.component import HTMLComponent
 from .header_model import HeaderModel
-from ..common.providers import HasPageService
+from nick_derobertis_site.common.providers import HasPageService
+from nick_derobertis_site.general.widgets.button import PrimaryButton
 
 
 class HeaderComponent(HasPageService, HTMLComponent):
     model = param.ClassSelector(class_=HeaderModel)
-    page_buttons: List[pn.widgets.Button]
+    page_buttons: List[PrimaryButton]
 
     def __init__(self, **params):
         self.page_buttons = []
         for page_name, page in self.page_service.routes.items():
-            button = pn.widgets.Button(name=page.model.page_link_text)
-            button.on_click(partial(self.navigate_to_page, page_name))
+            button = PrimaryButton(
+                display_text=page.model.page_link_text, page_path=page_name
+            )
             self.page_buttons.append(button)
 
         super().__init__(**params)
