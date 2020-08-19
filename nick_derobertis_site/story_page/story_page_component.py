31cde67f4d0623ba4995e57c2bb73489e08beedc
Nick DeRobertis <whoopnip@gmail.com>
2020-08-19 05:53:08 -0400

    Add link to software page on story page using new event element

--- nick_derobertis_site/story_page/story_page_component.py
+++ nick_derobertis_site/story_page/story_page_component.py
@@ -1,8 +1,20 @@
 import param
+from bokeh.events import Event
 
 from nick_derobertis_site.common.component import HTMLComponent
 from .story_page_model import StoryPageModel
+from nick_derobertis_site.common.event_elem import EventElement
+from nick_derobertis_site.common.providers import HasPageService
 
 
-class StoryPageComponent(HTMLComponent):
+class StoryPageComponent(HasPageService, HTMLComponent):
     model = param.ClassSelector(class_=StoryPageModel)
+    software_link = param.ClassSelector(class_=EventElement)
+
+    def __init__(self, **params):
+        params['software_link'] = EventElement(text='<a href="#">many of these tools.</a>', watch_events=['click'])
+        super().__init__(**params)
+        self.software_link.on('click', self.navigate_to_software_page)
+
+    def navigate_to_software_page(self, event: Event):
+        self.page_service.navigate('software')
