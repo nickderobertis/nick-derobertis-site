25350e72eea9dd126a421d298e6a119a8f57686c
Nick DeRobertis <whoopnip@gmail.com>
2020-08-19 06:01:09 -0400

    Switch card component panel buttons for custom event element buttons

--- nick_derobertis_site/landing/components/card/card_component.py
+++ nick_derobertis_site/landing/components/card/card_component.py
@@ -1,8 +1,11 @@
+from typing import Any, Dict
+
 import param
 import panel as pn
 from param.parameterized import Event
 
 from nick_derobertis_site.common.component import HTMLComponent
+from nick_derobertis_site.common.event_elem import EventElement
 from nick_derobertis_site.common.providers import HasPageService
 from nick_derobertis_site.landing.components.card.card_model import CardModel
 
@@ -11,9 +14,13 @@ class CardComponent(HasPageService, HTMLComponent):
     model: CardModel = param.ClassSelector(class_=CardModel)
 
     def __init__(self, **params):
-        self.button = pn.widgets.Button(name=params['model'].link_display_text, css_classes=['btn', 'btn-primary'])
-        self.button.on_click(self.navigate_to_link)
+        self.button = EventElement(text=self._button_html(params), watch_events=['click'])
+        self.button.on('click', self.navigate_to_link)
         super().__init__(**params)
+
 
     def navigate_to_link(self, event: Event):
         self.page_service.navigate(self.model.link)
+
+    def _button_html(self, params: Dict[str, Any]) -> str:
+        return f'<button class="btn btn-primary">{params["model"].link_display_text}</button>'
