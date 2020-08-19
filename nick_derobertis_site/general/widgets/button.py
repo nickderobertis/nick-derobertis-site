e208aedf201f82fd0cd248356850c348120658cc
Nick DeRobertis <whoopnip@gmail.com>
2020-08-19 06:20:59 -0400

    Refactor primary button in card component into general reusable widget

---
+++ nick_derobertis_site/general/widgets/button.py
@@ -0,0 +1,38 @@
+from typing import Dict, Any, Sequence
+
+import param
+from bokeh.events import Event
+
+from nick_derobertis_site.common.event_elem import EventElement
+from nick_derobertis_site.common.providers import HasPageService
+
+
+class ButtonBase(HasPageService, EventElement):
+    display_text = param.String()
+    page_path = param.String()
+    button_css_classes: Sequence[str] = ('btn',)
+
+    _rename = {**EventElement._rename, 'display_text': None, 'page_path': None}
+
+    def __init__(self, **params):
+        params['text'] = self._button_html(params)
+        params['watch_events'] = ['click']
+        self_params = dict(
+            display_text=params.pop('display_text'),
+            page_path=params.pop('page_path'),
+        )
+        super().__init__(**params)
+        all_params = {**dict(self.get_param_values()), **self_params}
+        param.Parameterized.__init__(self, **all_params)
+        self.on('click', self.navigate_to_page)
+
+    def _button_html(self, params: Dict[str, Any]) -> str:
+        css_class_str = ' '.join(self.button_css_classes)
+        return f'<button class="{css_class_str}">{params["display_text"]}</button>'
+
+    def navigate_to_page(self, event: Event):
+        self.page_service.navigate(self.page_path)
+
+
+class PrimaryButton(ButtonBase):
+    button_css_classes = tuple(ButtonBase.button_css_classes) + ('btn-primary',)
--- nick_derobertis_site/landing/components/card/card_component.py
+++ nick_derobertis_site/landing/components/card/card_component.py
@@ -7,6 +7,7 @@ from param.parameterized import Event
 from nick_derobertis_site.common.component import HTMLComponent
 from nick_derobertis_site.common.event_elem import EventElement
 from nick_derobertis_site.common.providers import HasPageService
+from nick_derobertis_site.general.widgets.button import PrimaryButton
 from nick_derobertis_site.landing.components.card.card_model import CardModel
 
 
@@ -14,13 +15,7 @@ class CardComponent(HasPageService, HTMLComponent):
     model: CardModel = param.ClassSelector(class_=CardModel)
 
     def __init__(self, **params):
-        self.button = EventElement(text=self._button_html(params), watch_events=['click'])
-        self.button.on('click', self.navigate_to_link)
+        self.button = PrimaryButton(
+            display_text=params["model"].link_display_text, page_path=params["model"].link
+        )
         super().__init__(**params)
-
-
-    def navigate_to_link(self, event: Event):
-        self.page_service.navigate(self.model.link)
-
-    def _button_html(self, params: Dict[str, Any]) -> str:
-        return f'<button class="btn btn-primary">{params["model"].link_display_text}</button>'
