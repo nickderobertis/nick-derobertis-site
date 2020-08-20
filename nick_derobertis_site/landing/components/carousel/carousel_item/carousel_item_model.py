3898ffbcbe4d45d5a7feda4c33928c61591347b3
Nick DeRobertis <whoopnip@gmail.com>
2020-08-20 17:27:12 -0400

    Add carousel images and styles

--- nick_derobertis_site/landing/components/carousel/carousel_item/carousel_item_model.py
+++ nick_derobertis_site/landing/components/carousel/carousel_item/carousel_item_model.py
@@ -1,6 +1,7 @@
 import param
 
 from nick_derobertis_site.common.model import ComponentModel
+from nick_derobertis_site.general.models.image import ImageModel
 from nick_derobertis_site.general.widgets.button import ButtonBase
 
 PLACEHOLDER_IMAGE = 'data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=='
@@ -11,7 +12,13 @@ class CarouselItemModel(ComponentModel):
     body_text = param.String()
     buttons = param.List(class_=ButtonBase)
     image_src = param.String(default=PLACEHOLDER_IMAGE)
+    image = param.ClassSelector(class_=ImageModel)
     caption_div_classes = param.List(class_=str, default=['carousel-caption'])
+
+    def __init__(self, **params):
+        if 'image_src' not in params and 'image' in params:
+            params['image_src'] = params['image'].src
+        super().__init__(**params)
 
     @property
     def caption_div_classes_str(self) -> str:
--- nick_derobertis_site/landing/config/carousel.py
+++ nick_derobertis_site/landing/config/carousel.py
@@ -1,11 +1,21 @@
+import os
+import pathlib
+
+from nick_derobertis_site.general.models.image import ImageModel
 from nick_derobertis_site.general.widgets.button import PrimaryButton
 from nick_derobertis_site.landing.components.carousel.carousel_item.carousel_item_model import CarouselItemModel
 from nick_derobertis_site.landing.components.carousel.carousel_model import CarouselModel
+
+IMAGES_PATH = pathlib.Path('nick_derobertis_site') / 'assets' / 'images'
 
 CAROUSEL_RESEARCH_BUTTON = PrimaryButton(display_text='Research', page_path='research')
 CAROUSEL_COURSES_BUTTON = PrimaryButton(display_text='Courses', page_path='courses')
 CAROUSEL_SOFTWARE_BUTTON = PrimaryButton(display_text='Software Projects', page_path='software')
 CAROUSEL_SKILLS_BUTTON = PrimaryButton(display_text='Skills', page_path='software')  # TODO: scroll to skills section
+
+DATA_SCIENCE_BANNER_IMAGE_MODEL = ImageModel(image_path=os.path.join(IMAGES_PATH, 'data-science-banner.jpg'))
+RESEARCH_IMAGE_MODEL = ImageModel(image_path=os.path.join(IMAGES_PATH, 'finance-research-banner.jpg'))
+TEACHING_IMAGE_MODEL = ImageModel(image_path=os.path.join(IMAGES_PATH, 'teaching-banner.jpg'))
 
 CAROUSEL_ITEM_MODELS = [
     CarouselItemModel(
@@ -13,11 +23,13 @@ CAROUSEL_ITEM_MODELS = [
         body_text='Focusing on market intervention, alternative assets, and behavioral finance',
         buttons=[CAROUSEL_RESEARCH_BUTTON],
         caption_div_classes=['carousel-caption', 'text-left'],
+        image=RESEARCH_IMAGE_MODEL,
     ),
     CarouselItemModel(
         header_text='Effective Teacher and Communicator',
         body_text='Experienced in teaching hundreds of students across multiple courses both in-person and online',
         buttons=[CAROUSEL_COURSES_BUTTON],
+        image=TEACHING_IMAGE_MODEL,
     ),
     CarouselItemModel(
         header_text='Data Scientist and Full-Stack Software Engineer',
@@ -25,6 +37,7 @@ CAROUSEL_ITEM_MODELS = [
                   'dozens of open-source packages, experienced in creating full web applications',
         buttons=[CAROUSEL_SKILLS_BUTTON, CAROUSEL_SOFTWARE_BUTTON],
         caption_div_classes=['carousel-caption', 'text-right'],
+        image=DATA_SCIENCE_BANNER_IMAGE_MODEL,
     )
 ]
 
