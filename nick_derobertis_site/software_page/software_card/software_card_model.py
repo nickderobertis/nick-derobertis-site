29cfe4d91fc22c1068ff75df6434c969aa5332eb
Nick DeRobertis <whoopnip@gmail.com>
2020-08-23 08:58:51 -0400

    Get software page fully working

--- nick_derobertis_site/software_page/software_card/software_card_model.py
+++ nick_derobertis_site/software_page/software_card/software_card_model.py
@@ -1,4 +1,5 @@
 import param
+from derobertis_cv.pltemplates.software.project import SoftwareProject
 
 from nick_derobertis_site.common.model import ComponentModel
 from nick_derobertis_site.general.utils import PLACEHOLDER_IMAGE
@@ -8,9 +9,22 @@ class SoftwareCardModel(ComponentModel):
     image_src: str = param.String(default=PLACEHOLDER_IMAGE)
     body_text: str = param.String(default='Placeholder body')
     header_text: str = param.String(default='Title')
+    small_header_text: str = param.String(default='Small Title')
     github_url: str = param.String(default='#')
     docs_url: str = param.String(default='#')
     accent_text: str = param.String(default='Accent')
+
+    @classmethod
+    def from_software_project(cls, project: SoftwareProject):
+        return cls(
+            body_text=project.description,
+            header_text=project.display_title,
+            small_header_text=project.title,
+            github_url=project.url or '',
+            image_src=project.logo_url or project.logo_base64 or '',
+            docs_url=project.docs_url or '',
+            accent_text=f'{project.commits} Commits, {project.loc:,.0f} LOC'
+        )
 
 
 
