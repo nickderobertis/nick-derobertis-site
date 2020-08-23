82a436f844b17a28359dc94be4a2e3b43d26189c
Nick DeRobertis <whoopnip@gmail.com>
2020-08-23 12:57:42 -0400

    Add placeholder image to software banner and restructure for better responsiveness

--- nick_derobertis_site/software_page/software_banner/software_banner_model.py
+++ nick_derobertis_site/software_page/software_banner/software_banner_model.py
@@ -1,8 +1,10 @@
 import param
 
 from nick_derobertis_site.common.model import ComponentModel
+from nick_derobertis_site.common.providers.image import HasImageModel
 
 
-class SoftwareBannerModel(ComponentModel):
+class SoftwareBannerModel(HasImageModel, ComponentModel):
     header_text: str = param.String()
     sub_text: str = param.String()
+
