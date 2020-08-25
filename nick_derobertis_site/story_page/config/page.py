76fb945fadd51fc0c1f95415552d34b30f036542
Nick DeRobertis <whoopnip@gmail.com>
2020-08-25 09:13:19 -0400

    Style story page

--- nick_derobertis_site/story_page/config/page.py
+++ nick_derobertis_site/story_page/config/page.py
@@ -1,6 +1,13 @@
+import os
+
+from nick_derobertis_site.general.config import IMAGES_PATH
+from nick_derobertis_site.general.models.image import ImageModel
 from nick_derobertis_site.story_page.story_page_model import StoryPageModel
+
+STORY_BACKGROUND_IMAGE_MODEL = ImageModel(image_path=os.path.join(IMAGES_PATH, 'story-cover.png'))
 
 STORY_PAGE_MODEL = StoryPageModel(
     page_title="Nick DeRobertis' Story",
-    page_link_text='Story'
+    page_link_text='Story',
+    image=STORY_BACKGROUND_IMAGE_MODEL
 )
--- nick_derobertis_site/story_page/story_page_model.py
+++ nick_derobertis_site/story_page/story_page_model.py
@@ -1,5 +1,6 @@
 from nick_derobertis_site.common.page_model import PageModel
+from nick_derobertis_site.common.providers.image import HasImageModel
 
 
-class StoryPageModel(PageModel):
+class StoryPageModel(HasImageModel, PageModel):
     pass
