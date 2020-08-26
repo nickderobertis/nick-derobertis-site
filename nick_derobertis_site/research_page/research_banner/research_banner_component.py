5804d8ff4eb53ccead648f364432698805a8cf03
Nick DeRobertis <whoopnip@gmail.com>
2020-08-26 12:58:53 -0400

    Add actual CV button to research page

--- nick_derobertis_site/research_page/research_banner/research_banner_component.py
+++ nick_derobertis_site/research_page/research_banner/research_banner_component.py
@@ -2,9 +2,12 @@ import param
 
 from nick_derobertis_site.common.component import HTMLComponent
 from .research_banner_model import ResearchBannerModel
+from nick_derobertis_site.general.config import CV_PDF_MODEL
+from nick_derobertis_site.general.widgets.button import PrimaryPDFButton
 
 
 class ResearchBannerComponent(HTMLComponent):
     model = param.ClassSelector(class_=ResearchBannerModel)
+    cv_button: PrimaryPDFButton = PrimaryPDFButton(display_text='View CV', pdf_src=CV_PDF_MODEL.src)
 
 
