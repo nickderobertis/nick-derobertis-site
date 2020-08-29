f4654a119620cd2023dc58697b9c989319862b8e
Nick DeRobertis <whoopnip@gmail.com>
2020-08-29 16:04:52 -0400

    Use configuration sub-models in creating task definition json to avoid needing all environment variables set

--- deploy/create_task_def.py
+++ deploy/create_task_def.py
@@ -4,11 +4,11 @@ import pathlib
 from jinja2 import Template
 
 try:
-    from .cdk_files.config import DeploymentConfig  # for running from root repo on CI
+    from .cdk_files.config import AWSSettings, DeploymentNames  # for running from root repo on CI
 except ImportError as e:
     if "attempted relative import with no known parent package" in str(e):
         # for running with deploy ./run-docker.sh to see actual output locally
-        from cdk_files.config import DeploymentConfig
+        from cdk_files.config import AWSSettings, DeploymentNames
     else:
         raise e
 
@@ -21,18 +21,19 @@ def _load_template(path: str = str(TEMPLATE_PATH)) -> str:
     return contents
 
 
-def _fill_values_in_template(template_str: str, model: DeploymentConfig) -> str:
+def _fill_values_in_template(template_str: str, aws_model: AWSSettings, names_model: DeploymentNames) -> str:
     tmpl = Template(template_str)
-    return tmpl.render(model.dict())
+    return tmpl.render(aws=aws_model.dict(), names=names_model.dict())
 
 
 def create_task_def_json(
     out_folder: str = ".",
     template_path: str = str(TEMPLATE_PATH),
-    model: DeploymentConfig = DeploymentConfig(),
+    aws_model: AWSSettings = AWSSettings(),
+    names_model: DeploymentNames = DeploymentNames(),
 ):
     template = _load_template(template_path)
-    templated = _fill_values_in_template(template, model=model)
+    templated = _fill_values_in_template(template, aws_model=aws_model, names_model=names_model)
     out_path = os.path.join(out_folder, "task-def.json")
     with open(out_path, "w") as f:
         f.write(templated)
