fec86627dac3bea9ed3eebceedb9931b089d4a97
Nick DeRobertis <whoopnip@gmail.com>
2020-08-30 16:23:08 -0400

    Get set up for separate SSH keys for each environment

--- deploy/container-scripts/initial-deploy.sh
+++ deploy/container-scripts/initial-deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+ENV_NAME=${1:-prod}
 MAIN_DEPLOY_NAME=$(python -m cdk_files.config -g names.app)
 ROUTE53_DEPLOY_NAME=$(python -m cdk_files.config -g names.route53_stack)
 
-echo "Deploying $MAIN_DEPLOY_NAME and $ROUTE53_DEPLOY_NAME"
+echo "Deploying environment $ENV_NAME. Stacks: $MAIN_DEPLOY_NAME and $ROUTE53_DEPLOY_NAME"
 
 cd deploy-cdk
-python -m create_ssh_key
+python -m create_ssh_key $ENV_NAME
 cp id_rsa ..
 cp id_rsa.pub ..
 cdk deploy $ROUTE53_DEPLOY_NAME
