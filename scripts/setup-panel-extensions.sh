0058d3164698eec9e16173232c99bab39685842f
Nick DeRobertis <whoopnip@gmail.com>
2020-08-30 18:32:41 -0400

    Add script to clone and build awesome panel extensions from github

---
+++ scripts/setup-panel-extensions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git clone --single-branch --branch templated-components https://github.com/nickderobertis/awesome-panel-extensions.git
+cd awesome-panel-extensions/awesome_panel_extensions
+npm install
+bokeh build
+cd ..
+mv awesome_panel_extensions ..
+cd ..
+rm -rf awesome-panel-extensions
