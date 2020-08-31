a0c6de8d798a1dfc2d2831d5de64c5828b9c7104
Nick DeRobertis <whoopnip@gmail.com>
2020-08-31 04:36:30 -0400

    Add setting up awesome panel extensions to main build script

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 
 sass nick_derobertis_site/styles.scss nick_derobertis_site/styles.css
 python -m nick_derobertis_site.gen_content.pdfs
+./scripts/setup-panel-extensions.sh
--- scripts/setup-panel-extensions.sh
+++ scripts/setup-panel-extensions.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -rf awesome_panel_extensions
 git clone --single-branch --branch templated-components https://github.com/nickderobertis/awesome-panel-extensions.git
 cd awesome-panel-extensions/awesome_panel_extensions
 npm install
