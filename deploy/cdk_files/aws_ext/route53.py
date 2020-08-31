672cbdfbcb9ad57857c6147b0ff458d7e5bd954b
Nick DeRobertis <whoopnip@gmail.com>
2020-08-31 07:47:49 -0400

    Restructure Route53 logic and add update to SOA record

--- deploy/cdk_files/aws_ext/route53.py
+++ deploy/cdk_files/aws_ext/route53.py
@@ -1,9 +1,16 @@
-from typing import List
+from typing import List, Sequence, Optional
 
 import boto3
 from boto3_type_annotations.route53 import Client as Route53Client
 
 ROUTE53_CLIENT: Route53Client = boto3.client("route53")
+
+DEFAULT_TTLS = {
+    "NS": 172800,
+    "CNAME": 300,
+    "TXT": 3600,
+    "SOA": 900,
+}
 
 
 def _list_hosted_zones(client: Route53Client = ROUTE53_CLIENT) -> List[dict]:
@@ -18,4 +25,38 @@ def get_hosted_zone_id_by_name(
     for zone in zones:
         if zone["Name"] in (name, name + "."):
             return zone["Id"]
-    raise ValueError(f"Could not find hosted zone with name {name}. Got names: {[zone['Name'] for zone in zones]}")
+    raise ValueError(
+        f"Could not find hosted zone with name {name}. Got names: {[zone['Name'] for zone in zones]}"
+    )
+
+
+def update_record(
+    url: str,
+    record_type: str,
+    values: Sequence[str],
+    ttl: Optional[int] = None,
+    client: Route53Client = ROUTE53_CLIENT,
+):
+    zone_id = get_hosted_zone_id_by_name(url)
+    if ttl is None:
+        ttl = DEFAULT_TTLS.get(record_type)
+
+    comment = f"Updated {record_type} record for {url} to {values}"
+
+    resource_record_set = {
+        "Name": url,
+        "Type": record_type,
+        "ResourceRecords": [{"Value": val} for val in values],
+    }
+    if ttl is not None:
+        resource_record_set.update(TTL=ttl)
+
+    client.change_resource_record_sets(
+        HostedZoneId=zone_id,
+        ChangeBatch={
+            "Comment": comment,
+            "Changes": [{"Action": "UPSERT", "ResourceRecordSet": resource_record_set}],
+        },
+    )
+
+    print(comment)
--- deploy/cdk_files/update_name_servers.py
+++ deploy/cdk_files/update_name_servers.py
@@ -1,7 +1,7 @@
 import boto3
 from boto3_type_annotations.route53 import Client as Route53Client
 
-from cdk_files.aws_ext.route53 import get_hosted_zone_id_by_name
+from cdk_files.aws_ext.route53 import get_hosted_zone_id_by_name, update_record
 from .config import DeploymentConfig
 
 ROUTE53_CLIENT: Route53Client = boto3.client("route53")
@@ -10,33 +10,20 @@ ROUTE53_CLIENT: Route53Client = boto3.client("route53")
 def update_ns_record(
     cfg: DeploymentConfig = DeploymentConfig(), client: Route53Client = ROUTE53_CLIENT
 ):
-    zone_id = get_hosted_zone_id_by_name(cfg.url)
-    comment = f"Updated NS records for {cfg.url} to {cfg.name_servers}"
+    update_record(cfg.url, "NS", cfg.name_servers, client=client)
 
-    client.change_resource_record_sets(
-        HostedZoneId=zone_id,
-        ChangeBatch={
-            "Comment": comment,
-            "Changes": [
-                {
-                    "Action": "UPSERT",
-                    "ResourceRecordSet": {
-                        "Name": cfg.url,
-                        "Type": "NS",
-                        "TTL": 172800,
-                        "ResourceRecords": [{"Value": ns} for ns in cfg.name_servers],
-                    },
-                }
-            ],
-        },
-    )
 
-    print(comment)
+def update_soa_record(
+    cfg: DeploymentConfig = DeploymentConfig(), client: Route53Client = ROUTE53_CLIENT
+):
+    soa_str = f"{cfg.name_servers[0]}. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400"
+    update_record(cfg.url, "SOA", [soa_str], client=client)
 
 
 if __name__ == "__main__":
     cfg = DeploymentConfig()
     if cfg.name_servers is not None:
         update_ns_record(cfg=cfg)
+        update_soa_record(cfg=cfg)
     else:
-        print('No name servers passed in config, will use auto-assigned name servers')
+        print("No name servers passed in config, will use auto-assigned name servers")
