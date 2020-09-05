729629cbffd83b449a6734ed9fc52acdb47eeb37
Nick DeRobertis <whoopnip@gmail.com>
2020-09-05 11:15:39 -0400

    Add buttons for other pages to contact section

--- nick_derobertis_site/landing/components/contact_pane/contact_pane_model.py
+++ nick_derobertis_site/landing/components/contact_pane/contact_pane_model.py
@@ -1,7 +1,11 @@
+from typing import List
+
 import param
 
 from nick_derobertis_site.common.model import ComponentModel
+from nick_derobertis_site.general.widgets.button import ButtonModel
 
 
 class ContactPaneModel(ComponentModel):
     email: str = param.String()
+    button_models: List[ButtonModel] = param.List(class_=ButtonModel)
--- nick_derobertis_site/landing/config/contact.py
+++ nick_derobertis_site/landing/config/contact.py
@@ -1,3 +1,10 @@
+from nick_derobertis_site.general.widgets.button import ButtonModel
 from nick_derobertis_site.landing.components.contact_pane.contact_pane_model import ContactPaneModel
 
-CONTACT_PANE_MODEL = ContactPaneModel(email='derobertisna@ufl.edu')
+CONTACT_BUTTON_MODELS = [
+    ButtonModel(display_text='See Research', page_path='research'),
+    ButtonModel(display_text='See Courses', page_path='courses'),
+    ButtonModel(display_text='See Software', page_path='software'),
+]
+
+CONTACT_PANE_MODEL = ContactPaneModel(email='derobertisna@ufl.edu', button_models=CONTACT_BUTTON_MODELS)
