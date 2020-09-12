1bdd35a33073293c1ea7a407a339fca84f8236a5
Nick DeRobertis <whoopnip@gmail.com>
2020-09-12 19:09:42 -0400

    Add story page content

--- frontend/nick-derobertis-site/src/app/story/story.module.ts
+++ frontend/nick-derobertis-site/src/app/story/story.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoryPageComponent } from './story-page/story-page.component';
-
-
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [StoryPageComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, RouterModule],
 })
-export class StoryModule { }
+export class StoryModule {}
