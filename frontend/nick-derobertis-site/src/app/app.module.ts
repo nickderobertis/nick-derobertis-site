6b04eeb0173cb331bde47cc76ef959b02d5e2274
Nick DeRobertis <whoopnip@gmail.com>
2020-09-12 17:02:21 -0400

    Add footer

--- frontend/nick-derobertis-site/src/app/app.module.ts
+++ frontend/nick-derobertis-site/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderModule } from './header/header.module';
+import { FooterModule } from './footer/footer.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -11,6 +12,7 @@ import { HeaderModule } from './header/header.module';
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     HeaderModule,
+    FooterModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ frontend/nick-derobertis-site/src/app/footer/footer/footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { copyright } from 'src/app/global/classes/copyright';
+
+@Component({
+  selector: 'nds-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+})
+export class FooterComponent implements OnInit {
+  copyright: string = copyright.str;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
