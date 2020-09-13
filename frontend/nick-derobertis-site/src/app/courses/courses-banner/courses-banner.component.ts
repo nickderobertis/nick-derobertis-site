683ac8d29361503256bb35e472febd161014cf9f
Nick DeRobertis <whoopnip@gmail.com>
2020-09-13 13:21:57 -0400

    Add courses banner

---
+++ frontend/nick-derobertis-site/src/app/courses/courses-banner/courses-banner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nds-courses-banner',
+  templateUrl: './courses-banner.component.html',
+  styleUrls: ['./courses-banner.component.scss']
+})
+export class CoursesBannerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- frontend/nick-derobertis-site/src/app/courses/courses.module.ts
+++ frontend/nick-derobertis-site/src/app/courses/courses.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoursesPageComponent } from './courses-page/courses-page.component';
+import { CoursesBannerComponent } from './courses-banner/courses-banner.component';
 
 
 
 @NgModule({
-  declarations: [CoursesPageComponent],
+  declarations: [CoursesPageComponent, CoursesBannerComponent],
   imports: [
     CommonModule
   ]
