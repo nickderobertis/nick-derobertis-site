7d6b4da85298db76f2603d8368951f4c29b15ced
Nick DeRobertis <whoopnip@gmail.com>
2020-09-13 11:19:03 -0400

    Add software banner

--- frontend/nick-derobertis-site/src/app/software/software.module.ts
+++ frontend/nick-derobertis-site/src/app/software/software.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SoftwarePageComponent } from './software-page/software-page.component';
+import { SoftwareBannerComponent } from './software-banner/software-banner.component';
 
 
 
 @NgModule({
-  declarations: [SoftwarePageComponent],
+  declarations: [SoftwarePageComponent, SoftwareBannerComponent],
   imports: [
     CommonModule
   ]
