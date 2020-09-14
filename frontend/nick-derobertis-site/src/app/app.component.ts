a57da30d9f6a2737f29241b31c10059e81682a11
Nick DeRobertis <whoopnip@gmail.com>
2020-09-13 20:57:11 -0400

    WIP on google analytics, getting gtag not defined

--- frontend/nick-derobertis-site/src/app/app.component.ts
+++ frontend/nick-derobertis-site/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'nick-derobertis-site';
+
+  constructor(
+    public router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    // subscribe to router events and send page views to Google Analytics
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        if (isPlatformBrowser(this.platformId)) {
+          gtag('set', 'page', event.urlAfterRedirects);
+          gtag('send', 'pageview');
+        }
+      }
+    });
+  }
 }
