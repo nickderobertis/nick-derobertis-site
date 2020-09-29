c8ca7bf5d7acea0332817fcf6dce29c973719699
Nick DeRobertis <whoopnip@gmail.com>
2020-09-29 10:58:40 -0400

    Auto chart height

--- frontend/nick-derobertis-site/src/app/home/timeline-pane/timeline-widget/timelines-model.ts
+++ frontend/nick-derobertis-site/src/app/home/timeline-pane/timeline-widget/timelines-model.ts
@@ -2,6 +2,10 @@ import { APITimelineResponseModel } from 'src/app/global/interfaces/generated/ti
 import { TimelineDataRow } from './timeline-data-row';
 import { TimelineModel } from './timeline-model';
 
+const HEIGHT_PER_SINGLE_ROW: number = 40.99;
+const HEIGHT_PER_DOUBLE_ROW: number = 70.98;
+const HEIGHT_PADDING: number = 30;
+
 export class TimelinesModel {
   timelines: TimelineModel[];
   chartColumns: {
@@ -15,9 +19,11 @@ export class TimelinesModel {
     { type: 'date', id: 'Start' },
     { type: 'date', id: 'End' },
   ];
+  chartHeight: number;
 
   constructor(args: APITimelineResponseModel) {
     this.timelines = TimelineModel.arrayFromAPIArray(args.items);
+    this.chartHeight = chartHeightFromTimelines(this.timelines);
   }
 
   toChartData(): TimelineDataRow[] {
@@ -28,4 +34,95 @@ export class TimelinesModel {
     }
     return data;
   }
+}
+
+interface TimeRangeCount {
+  begin: Date;
+  end: Date;
+  count: number;
+}
+
+interface TimeRangesCounts {
+  [organization: string]: TimeRangeCount[];
+}
+
+function chartHeightFromTimelines(timelines: TimelineModel[]): number {
+  const organizationRanges: TimeRangesCounts = chartRangesFromTimelines(
+    timelines
+  );
+  let height: number = HEIGHT_PADDING;
+  for (const organization of Object.keys(organizationRanges)) {
+    const ranges = organizationRanges[organization];
+    for (const range of ranges) {
+      if (range.count === 1) {
+        height += HEIGHT_PER_SINGLE_ROW;
+      } else if (range.count === 2) {
+        height += HEIGHT_PER_DOUBLE_ROW;
+      } else {
+        console.warn(
+          `no handling for ${range.count} number per row in timeline chart`
+        );
+        height += HEIGHT_PER_DOUBLE_ROW;
+      }
+    }
+  }
+  return height;
+}
+
+function chartRangesFromTimelines(
+  timelines: TimelineModel[]
+): TimeRangesCounts {
+  const ranges: TimeRangesCounts = {};
+  for (const timeline of timelines) {
+    const existingRanges = ranges[timeline.organization] || [];
+    let rangeMatched: boolean = false;
+    for (const existingRange of existingRanges) {
+      if (
+        existingRange.begin &&
+        dateRangeOverlaps(
+          existingRange.begin,
+          existingRange.end,
+          timeline.beginDate,
+          timeline.timelineEndDate
+        )
+      ) {
+        existingRange.count += 1;
+        existingRange.begin = new Date(
+          Math.min.apply(null, [existingRange.begin, timeline.beginDate])
+        );
+        existingRange.end = new Date(
+          Math.max.apply(null, [existingRange.end, timeline.timelineEndDate])
+        );
+        rangeMatched = true;
+      }
+    }
+    if (!rangeMatched) {
+      const newRange = {
+        begin: timeline.beginDate,
+        end: timeline.timelineEndDate,
+        count: 1,
+      };
+      existingRanges.push(newRange);
+    }
+    ranges[timeline.organization] = existingRanges;
+  }
+  return ranges;
+}
+
+function dateRangeOverlaps(
+  aStart: Date,
+  aEnd: Date,
+  bStart: Date,
+  bEnd: Date
+): boolean {
+  if (aStart <= bStart && bStart <= aEnd) {
+    return true;
+  } // b starts in a
+  if (aStart <= bEnd && bEnd <= aEnd) {
+    return true;
+  } // b ends in a
+  if (bStart < aStart && aEnd < bEnd) {
+    return true;
+  } // a in b
+  return false;
 }
