730a7115be5e0072bdea1d70cbcc1d313eaf55e2
Nick DeRobertis <whoopnip@gmail.com>
2020-09-30 11:50:35 -0400

    Polish colors in timeline widget

--- frontend/nick-derobertis-site/src/app/home/timeline-pane/timeline-widget/timeline-color-pair.ts
+++ frontend/nick-derobertis-site/src/app/home/timeline-pane/timeline-widget/timeline-color-pair.ts
@@ -3,16 +3,16 @@ export interface TimelineColorPair {
   text: string;
 }
 
-// https://coolors.co/273043-9197ae-eff6ee-f02d3a-dd0426-ad343e-f2af29-a98743-f68e5f-f76c5e
+// https://coolors.co/273043-9c6615-c2aff0-6e0e0a-c45baa-ad343e-d63230-a755c2-9a031e-ef476f
 export const TIMELINE_COLORS: TimelineColorPair[] = [
   { background: '#273043', text: 'white' },
-  { background: '#9197AE', text: 'white' },
-  { background: '#EFF6EE', text: 'black' },
-  { background: '#F02D3A', text: 'white' },
-  { background: '#DD0426', text: 'white' },
+  { background: '#9c6615', text: 'white' },
+  { background: '#c2aff0', text: 'black' },
+  { background: '#6e0e0a', text: 'white' },
+  { background: '#c45baa', text: 'white' },
   { background: '#AD343E', text: 'white' },
-  { background: '#F2AF29', text: 'black' },
-  { background: '#A98743', text: 'white' },
-  { background: '#F68E5F', text: 'black' },
-  { background: '#F76C5E', text: 'white' },
+  { background: '#d63230', text: 'white' },
+  { background: '#a755c2', text: 'white' },
+  { background: '#9a031e', text: 'white' },
+  { background: '#ef476f', text: 'white' },
 ];
