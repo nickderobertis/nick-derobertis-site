b0893f995fe6a99edbdd1a983d20911f686bf332
Nick DeRobertis <whoopnip@gmail.com>
2020-10-05 20:11:42 -0400

    Make timeline section say tap instead of hover on touch devices

--- frontend/nick-derobertis-site/src/app/home/timeline-pane/timeline-pane.component.ts
+++ frontend/nick-derobertis-site/src/app/home/timeline-pane/timeline-pane.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { isTouchDevice } from 'src/app/global/functions/browser';
 
 @Component({
   selector: 'nds-timeline-pane',
@@ -7,8 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimelinePaneComponent implements OnInit {
   loading: boolean = false; // TODO: get and use stats, then loading becomes useful
+  actionWord: string = 'Hover over';
 
-  constructor() {}
+  constructor(@Inject(PLATFORM_ID) private readonly platformId: object) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId) && isTouchDevice()) {
+      this.actionWord = 'Tap';
+    }
+  }
 }
