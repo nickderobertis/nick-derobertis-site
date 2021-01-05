54384fe2e7f8d0a31143eda6bd77d4552e6e1369
Nick DeRobertis <whoopnip@gmail.com>
2021-01-04 19:48:38 -0500

    Get hours, first used, priorities into SkillModel on the frontend

--- frontend/nick-derobertis-site/src/app/skills/skills-widget/skills-dropdown/skill-model.ts
+++ frontend/nick-derobertis-site/src/app/skills/skills-widget/skills-dropdown/skill-model.ts
@@ -1,9 +1,19 @@
-import { APISkillModel } from 'src/app/global/interfaces/generated/skills';
+import {
+  APISkillModel,
+  ApplicationFocus,
+  SpecificApplicationFocus,
+} from 'src/app/global/interfaces/generated/skills';
+
+export type AllApplicationFocus = ApplicationFocus | SpecificApplicationFocus;
+type Priorities = { [focus in AllApplicationFocus]: number };
 
 export class SkillModel {
   title: string;
   level: number;
   directParentTitle?: string;
+  hours?: number;
+  firstUsed?: Date;
+  priorities: Priorities;
   levelNames: { [level: number]: string } = {
     1: 'Have Used',
     2: 'Some Experience',
@@ -15,8 +25,15 @@ export class SkillModel {
   constructor(args: APISkillModel) {
     this.title = args.title;
     this.level = args.level;
+    this.priorities = args.priorities as Priorities;
     if (args.direct_parent_title) {
       this.directParentTitle = args.direct_parent_title;
+    }
+    if (args.hours) {
+      this.hours = args.hours;
+    }
+    if (args.first_used) {
+      this.firstUsed = new Date(args.first_used);
     }
   }
 
