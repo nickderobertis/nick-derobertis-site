488727f76086432dc15cc16fb4197aa38ea42cf2
Nick DeRobertis <whoopnip@gmail.com>
2023-12-23 16:28:33 -0600

    chore: Don't expose port 22 on docker

--- run-docker.sh
+++ run-docker.sh
@@ -2,7 +2,7 @@
 
 source ./.env.sh || echo "No .env.sh file found"
 
-docker run -p 80:80 -p 22:22 \
+docker run -p 80:80 \
   -e "NDS_ENVIRONMENT_NAME=$NDS_ENVIRONMENT_NAME" \
   -e "BE_SENTRY_DSN=$BE_SENTRY_DSN" \
   nick-derobertis-site
