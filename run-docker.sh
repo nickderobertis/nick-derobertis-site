53a1420a59bdcff4fbaf9bb9576fd90ceb321e5d
Nick DeRobertis <whoopnip@gmail.com>
2023-12-23 16:16:46 -0600

    chore: More cleanup of ssh capability

--- run-docker.sh
+++ run-docker.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-source ./.env.sh
-
-# TODO [#8]: auto create public key, right now requires using deployment container
-SSH_PUBLIC_KEY=$(cat ./deploy/id_rsa.pub);
+source ./.env.sh || echo "No .env.sh file found"
 
 docker run -p 80:80 -p 22:22 \
-  -e "SSH_PUBLIC_KEY=$SSH_PUBLIC_KEY" \
   -e "NDS_ENVIRONMENT_NAME=$NDS_ENVIRONMENT_NAME" \
   -e "BE_SENTRY_DSN=$BE_SENTRY_DSN" \
   nick-derobertis-site
