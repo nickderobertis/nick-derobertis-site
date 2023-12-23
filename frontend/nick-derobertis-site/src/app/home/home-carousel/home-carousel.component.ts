299ff49400bf09831d50e6b9ff33f47ade34a89e
Nick DeRobertis <whoopnip@gmail.com>
2023-12-22 19:02:08 -0600

    feat: Update copy for changes to 12/2023

--- frontend/nick-derobertis-site/src/app/home/home-carousel/home-carousel.component.ts
+++ frontend/nick-derobertis-site/src/app/home/home-carousel/home-carousel.component.ts
@@ -10,19 +10,19 @@ import { HomeCarouselItemModel } from './home-carousel-item/home-carousel-item-m
 export class HomeCarouselComponent implements OnInit {
   itemModels: HomeCarouselItemModel[] = [
     new HomeCarouselItemModel({
+      headerText: 'Serial Founder & Full-Stack Software Engineer',
+      bodyText:
+        'CTO of multiple startups, experienced in creating full web and mobile applications, maintainer of dozens of open-source packages',
+      imageHref: 'assets/images/software-engineering-banner.jpg',
+      links: [{ link: 'software', title: 'Software Projects' }],
+    }),
+    new HomeCarouselItemModel({
       headerText: 'Finance Ph.D. Researcher',
       bodyText:
         'Research focuses on market intervention, alternative assets, and behavioral finance',
       links: [{ link: 'research', title: 'Research' }],
       captionClasses: ['carousel-caption', 'text-left'],
       imageHref: 'assets/images/finance-research-banner.jpg',
-    }),
-    new HomeCarouselItemModel({
-      headerText: 'Effective Teacher and Communicator',
-      bodyText:
-        'Experienced in teaching hundreds of students across multiple courses both in-person and online',
-      imageHref: 'assets/images/teaching-banner.jpg',
-      links: [{ link: 'courses', title: 'Courses' }],
     }),
     new HomeCarouselItemModel({
       headerText: 'Data Scientist',
@@ -33,11 +33,11 @@ export class HomeCarouselComponent implements OnInit {
       fragmentLinks: [{ link: 'skills', title: 'Skills' }],
     }),
     new HomeCarouselItemModel({
-      headerText: 'Full-Stack Software Engineer',
+      headerText: 'Effective Teacher and Communicator',
       bodyText:
-        'Maintainer of dozens of open-source packages, experienced in creating full web and mobile applications',
-      imageHref: 'assets/images/software-engineering-banner.jpg',
-      links: [{ link: 'software', title: 'Software Projects' }],
+        'Experienced in teaching hundreds of students across multiple courses both in-person and online',
+      imageHref: 'assets/images/teaching-banner.jpg',
+      links: [{ link: 'courses', title: 'Courses' }],
     }),
   ];
 
--- frontend/nick-derobertis-site/src/app/home/home-page/home-page.component.ts
+++ frontend/nick-derobertis-site/src/app/home/home-page/home-page.component.ts
@@ -9,6 +9,27 @@ import { HomeCardModel } from '../home-card/home-card-model';
 export class HomePageComponent implements OnInit {
   cardModels: HomeCardModel[] = [
     new HomeCardModel({
+      heading: 'Engineering',
+      bodyText: `
+      Whether it is building an application,
+      solving a data science problem, or
+      finding the best way to teach a topic, I optimize my approach
+      and usually build open-source tools along the way.
+      `,
+      link: { link: 'software', title: 'View Software' },
+      iconClasses: ['fas', 'fa-cogs', 'fa-5x'],
+    }),
+    new HomeCardModel({
+      heading: 'Teaching',
+      bodyText: `
+      I've taught multiple courses at multiple universities.
+      Most recently, I taught Financial Modeling using
+      Python and Excel at the University of Florida.
+      `,
+      link: { link: 'courses', title: 'View Courses' },
+      iconClasses: ['fas', 'fa-graduation-cap', 'fa-5x'],
+    }),
+    new HomeCardModel({
       heading: 'Research',
       bodyText: `
       I am an empirical researcher working in the finance field.
@@ -17,27 +38,6 @@ export class HomePageComponent implements OnInit {
       `,
       link: { link: 'research', title: 'View Research' },
       iconClasses: ['fas', 'fa-chart-bar', 'fa-5x'],
-    }),
-    new HomeCardModel({
-      heading: 'Teaching',
-      bodyText: `
-      I've taught multiple courses at multiple universities.
-      Currently I am teaching Financial Modeling using
-      Python and Excel at the University of Florida.
-      `,
-      link: { link: 'courses', title: 'View Courses' },
-      iconClasses: ['fas', 'fa-graduation-cap', 'fa-5x'],
-    }),
-    new HomeCardModel({
-      heading: 'Engineering',
-      bodyText: `
-      Whether it is a data science problem,
-      finding the best way to teach a topic, or
-      building a web app, I optimize my approach
-      and usually build open-source tools along the way.
-      `,
-      link: { link: 'software', title: 'View Software' },
-      iconClasses: ['fas', 'fa-cogs', 'fa-5x'],
     }),
   ];
 
